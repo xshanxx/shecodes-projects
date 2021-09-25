93882347361ab25b9349d4f065ffdd3786a89113
xshanxx <88444699+xshanxx@users.noreply.github.com>
2021-09-26 01:21:37 +0800

    added apiUrl for coordinates for forecast

--- src/index.js
+++ src/index.js
@@ -39,7 +39,8 @@ function formatDate(timestamp) {
   return `${day}, ${today} ${month} ${year}, ${hours}:${mins}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row justify-content-center">`;
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
@@ -63,6 +64,13 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "b3e76648f469c0e9420b4566725bb244";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -131,4 +139,3 @@ let celsiusLink = document.querySelector("#cel-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("New York");
-displayForecast();
